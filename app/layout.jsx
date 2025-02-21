420c349935fb278504e6f955effe2d43ea2b8cd0
John Doe <john.doe@example.com>
2025-02-21 18:34:11 +0300

    test update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
 import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
 
 export const metadata = {
     title: {
@@ -13,7 +11,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
             <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
+                <link rel="icon" href="#" sizes="any" />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
